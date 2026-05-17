73c11bd66bce7a30c714724584f8c680cb205310
Shabbir Hasan <68828793+ShabbirHasan1@users.noreply.github.com>
2026-05-17 15:14:15 +0530

    Add support for smol_str
    Introduce an optional `smol_str` feature that enables `SmolStr` support
    via a new macro-based implementation. This allows for optimized,
    heap-free encoding and decoding of short strings.

--- src/str.rs
+++ src/str.rs
@@ -57,20 +57,51 @@ impl<'b> Encoder<&'b str> for StrEncoder {
     }
 }
 
-impl Encoder<String> for StrEncoder {
-    #[inline(always)]
-    fn encode(&mut self, t: &String) {
-        self.encode(t.as_str());
-    }
+// impl Encoder<String> for StrEncoder {
+//     #[inline(always)]
+//     fn encode(&mut self, t: &String) {
+//         self.encode(t.as_str());
+//     }
 
-    #[inline(always)]
-    fn encode_vectored<'a>(&mut self, i: impl Iterator<Item = &'a String> + Clone)
-    where
-        String: 'a,
-    {
-        self.encode_vectored(i.map(String::as_str));
-    }
+//     #[inline(always)]
+//     fn encode_vectored<'a>(&mut self, i: impl Iterator<Item = &'a String> + Clone)
+//     where
+//         String: 'a,
+//     {
+//         self.encode_vectored(i.map(String::as_str));
+//     }
+// }
+
+macro_rules! impl_string {
+    ($t:ty) => {
+        impl crate::coder::Encoder<$t> for crate::str::StrEncoder {
+            #[inline(always)]
+            fn encode(&mut self, t: &$t) {
+                self.encode(t.as_str());
+            }
+
+            #[inline(always)]
+            fn encode_vectored<'a>(&mut self, i: impl Iterator<Item = &'a $t> + Clone)
+            where
+                $t: 'a,
+            {
+                self.encode_vectored(i.map(|s| s.as_str()));
+            }
+        }
+
+        impl<'a> crate::coder::Decoder<'a, $t> for crate::str::StrDecoder<'a> {
+            #[inline(always)]
+            fn decode(&mut self) -> $t {
+                let v: &str = self.decode();
+                <$t>::from(v)
+            }
+        }
+    };
 }
+
+impl_string!(String);
+#[cfg(feature = "smol_str")]
+impl_string!(smol_str::SmolStr);
 
 // Doesn't use VecDecoder because can't decode &[u8].
 #[derive(Default)]
@@ -131,13 +162,13 @@ impl<'a> Decoder<'a, &'a str> for StrDecoder<'a> {
     }
 }
 
-impl<'a> Decoder<'a, String> for StrDecoder<'a> {
-    #[inline(always)]
-    fn decode(&mut self) -> String {
-        let v: &str = self.decode();
-        v.to_owned()
-    }
-}
+// impl<'a> Decoder<'a, String> for StrDecoder<'a> {
+//     #[inline(always)]
+//     fn decode(&mut self) -> String {
+//         let v: &str = self.decode();
+//         v.to_owned()
+//     }
+// }
 
 /// Tests 128 bytes a time instead of `<[u8]>::is_ascii` which only tests 8.
 /// 390% faster on 8KB, 27% faster on 1GB (RAM bottleneck).
@@ -287,4 +318,27 @@ mod tests2 {
             .collect()
     }
     crate::bench_encode_decode!(str_vec: Vec<String>);
+}
+
+#[cfg(all(test, feature = "smol_str"))]
+mod smol_str_tests {
+    use smol_str::SmolStr;
+
+    /// Short strings stay inline after decode (no heap allocation).
+    #[test]
+    fn decoded_short_string_is_not_heap_allocated() {
+        let s = SmolStr::new("hello");
+        assert!(!s.is_heap_allocated());
+        let decoded: SmolStr = crate::decode(&crate::encode(&s)).unwrap();
+        assert!(!decoded.is_heap_allocated());
+    }
+
+    /// Long strings are heap-allocated after decode.
+    #[test]
+    fn decoded_long_string_is_heap_allocated() {
+        let s = SmolStr::new("this is a longer string that exceeds inline storage");
+        assert!(s.is_heap_allocated());
+        let decoded: SmolStr = crate::decode(&crate::encode(&s)).unwrap();
+        assert!(decoded.is_heap_allocated());
+    }
 }
